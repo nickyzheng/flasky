29ed29e06e05104ee716af7f70b2dd3b7ecc28fc
nickyzj <reg.zj@outlook.com>
2015-06-08 17:23:05 +0800

    end of chapter 4

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+app.config['SECRET_KEY'] = 'hard to guss string'
+
+class NameForm(Form):
+	name = StringField('What is your name?', validators = [Required()])
+	submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,9 +26,13 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time = datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form = form, name = session.get('name'), current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
